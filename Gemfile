15bbe49856669be22664f055531b22b2ad2b6557
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-06-11 22:01:10 -0300

    Add devise

--- Gemfile
+++ Gemfile
@@ -34,7 +34,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'rspec-rails'
 gem 'rest-client'
-gem 'oauth'
+gem 'devise'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'pocket/connect', to: 'pocket_account#connect', as: :connect_pocket
   get 'pocket/new', to: 'pocket_account#new', as: :new_pocket_account
   get 'pocket-callback', to: 'pocket_account#callback', as: :pocket_callback
